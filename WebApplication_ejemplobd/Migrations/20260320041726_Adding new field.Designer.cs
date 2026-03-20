53346f06a9901c4bfd711b34b3e1d4d76e12c387
Valeria Ponce <114198256+imvaleriaponce@users.noreply.github.com>
2026-03-19 22:21:29 -0600

    Adding new field

--- WebApplication_ejemplobd/Migrations/20260320041726_Adding new field.Designer.cs
+++ WebApplication_ejemplobd/Migrations/20260320041726_Adding new field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication_ejemplobd.Data;
 
@@ -11,9 +12,11 @@ using WebApplication_ejemplobd.Data;
 namespace WebApplication_ejemplobd.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260320041726_Adding new field")]
+    partial class Addingnewfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
