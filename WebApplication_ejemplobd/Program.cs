4c859d31d8f50c4b157c022b9bc0a212cb13ac11
Tavo0402 <59376770+Tavo0402@users.noreply.github.com>
2026-05-18 16:52:38 -0600

    Fix docker image build errors

--- WebApplication_ejemplobd/Program.cs
+++ WebApplication_ejemplobd/Program.cs
@@ -6,7 +6,15 @@ using WebApplication_ejemplobd.Repositories.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORS", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,14 +25,18 @@ builder.Services.AddScoped<IToDoRepository, ToDoRepository>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
+app.UseCors("CORS");
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
